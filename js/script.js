51b587d9dda052333a3ae8670cf7c47c46062c37
loukensclaude <l.claude@ihsph.org>
2019-11-19 23:11:44 +0000

    Improve my code and add backgroung color

--- js/script.js
+++ js/script.js
@@ -10,15 +10,61 @@ $("document").ready(function() {
 
     
 
-    $("#shoot").click (function() {
-        let num = [1, 2, 3];
-        
-        let word = $("#input").val();
+$(".button").click (function() {
+      
+    let possible_outcomes = ['rock', 'paper', 'scissor'];
 
-        let theNum = num.random();
-        
-        $("#userChoice").text("theNum");
+    let word = $(".input").val();
 
+    let random_number = Math.floor(Math.random()* possible_outcomes.length);
+
+    let computer_choice = possible_outcomes[random_number];
+        
+        $(".output").text(word);
+        
+        $(".input").val("");
+
+        $(".output2").text(computer_choice);
+
+        if (word == "rock" && computer_choice == "rock") {
+          $(".output3").text("You are tied");
+          $("body").css("background", "gray")
+        }
+        else if (word == "rock" && computer_choice == "paper") {
+          $(".output3").text("You lose");
+          $("body").css("background", "red")
+        }
+        else if (word == "rock" && computer_choice == "scissor") {
+          $(".output3").text("You Win");
+          $("body").css("background", "green")
+        }
+        else if (word == "paper" && computer_choice == "rock") {
+          $(".output3").text("You Win");
+          $("body").css("background", "green")
+        }
+        else if (word == "paper" && computer_choice == "paper") {
+          $(".output3").text("You tied");
+          $("body").css("background", "gray")
+        }
+        else if (word == "paper" && computer_choice == "scissor") {
+          $(".output3").text("You lose");
+          $("body").css("background", "red")
+        }
+        else if (word == "scissor" && computer_choice == "rock") {
+          $(".output3").text("You lose");
+          $("body").css("background", "red")
+        }
+        else if (word == "scissor" && computer_choice == "paper") {
+          $(".output3").text("You Win");
+          $("body").css("background", "green")
+        }
+        else if (word == "scissor" && computer_choice == "scissor") {
+          $(".output3").text("You tied");
+          $("body").css("background", "gray")
+        }
+        else {
+          alert("There seems to be an error. Please check if your choice is spelled correctly.");
+        }
     });
 
 //     git status
@@ -31,3 +77,4 @@ $("document").ready(function() {
 
 // DOCUMENT READY FUNCTION BELOW
 
+// python3 -m http.server 3000
